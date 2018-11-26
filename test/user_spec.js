5cdf1dd6a4ef979710cce2e007d171ba6e349dc0
Ashwini Jindal <ashwini.jindal@securemeters.com>
2018-11-26 11:40:13 +0530

    add test file

---
+++ test/user_spec.js
@@ -0,0 +1,12 @@
+var should= require("should");
+
+describe("test1", function(){
+    
+    describe("memebership test 1 ", function(){
+        it("is all normal", function(){
+            assert.ok(1==1);
+        });
+    });
+
+
+});
