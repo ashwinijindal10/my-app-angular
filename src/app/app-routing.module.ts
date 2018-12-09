501680793a21940255fb2bcc904754a08b2938a5
ashwini jindal <ashwinijindal10@gmail.com>
2018-12-09 17:03:30 +0530

    updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MydashboardComponent } from './mydashboard/mydashboard.component';
+import { MyaddressComponent } from './myaddress/myaddress.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'dashboard', component: MydashboardComponent },
+  { path: 'add-address', component: MyaddressComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
